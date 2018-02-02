5a047f79957021f3f5420fca6d7a75a61aef70b8
Unknown <carlosgonz817@gmail.com>
2018-02-01 23:04:17 -0800

    Auto stash before merge of "master" and "web-dev/master"

---
+++ assignment4/scripts/script.js
@@ -0,0 +1,46 @@
+$( document ).ready(function()
+//code for sub menu on hover
+  $(function() {
+    $('ul > li').hover( function(){
+      $(this).find('ul.sub1').show(200);
+    }, function() {
+      $(this).find('ul.sub1').hide(200);
+    });
+  });
+  //slide show code
+
+
+
+
+  //code for anything related to paragraphs
+  $('#des').on('dblclick', function(){
+    $('#des').css("display","none");
+    $('#textarea1, #buttonOne').css("display","block");
+  });
+  $('#buttonOne').on('click', function(){
+    $('#textarea1, #buttonOne').css("display","none");
+    $('#des').html($('#textarea1').val());
+    $('#des').css("display","block");
+});
+  $('#des-two').on('dblclick', function(){
+    $('#des-two').css("display","none");
+    $('#textarea2, #buttonTwo').css("display","block");
+  });
+    $('#buttonTwo').on('click', function(){
+      $('#textarea2, #buttonTwo').css("display","none");
+      $('#des-two').html($('#textarea2').val());
+      $('#des-two').css("display","block");
+  });
+//code for anything related to tiles
+  $('#resetAll').on('click', function(){
+    $(".tiles").css('display','block');
+  });
+  $('#clearAll').on('click', function(){
+    $(".tiles").css('display','none');
+  });
+
+  $('.tiles').on('click', function(tile){
+		$(this).fadeOut('1000', function(tile){
+		});
+	});
+});//end of ready function
