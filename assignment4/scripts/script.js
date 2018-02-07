d2039091a2295e56a860323605f3b3a98ade5d51
Bryce Graves <brycegraves97@gmail.com>
2018-02-07 15:17:24 -0800

    Format Changes

--- assignment4/scripts/script.js
+++ assignment4/scripts/script.js
@@ -1,50 +1,53 @@
-$( document ).ready(function() {
-//code for sub menu on hover
+$(document).ready(function() {
+  //code for sub menu on hover
   $(function() {
-    $('ul > li').hover( function() {
+    $('ul > li').hover(function() {
       $(this).find('ul.sub1').show(200);
     }, function() {
       $(this).find('ul.sub1').hide(200);
     });
   });
+
   //slideshow code
   $(function() {
-      $('.prezi-content img:gt(0)').hide();
-      setInterval(function() {
+    $('.prezi-content img:gt(0)').hide();
+    setInterval(function() {
         $('.prezi-content :first-child').fadeOut()
-           .next('img').fadeIn()
-           .end().appendTo('.prezi-content');},
-        3000);
+          .next('img').fadeIn()
+          .end().appendTo('.prezi-content');
+      },
+      3000);
   });
+
   //code for anything related to paragraphs
   $('#des').on('dblclick', function() {
-    $('#des').css("display","none");
-    $('#textarea1, #buttonOne').css("display","block");
+    $('#des').css("display", "none");
+    $('#textarea1, #buttonOne').css("display", "block");
   });
   $('#buttonOne').on('click', function() {
-    $('#textarea1, #buttonOne').css("display","none");
+    $('#textarea1, #buttonOne').css("display", "none");
     $('#des').html($('#textarea1').val());
-    $('#des').css("display","block");
-  });/////////////////////////////////////////////////////////;
+    $('#des').css("display", "block");
+  });
   $('#des-two').on('dblclick', function() {
-    $('#des-two').css("display","none");
-    $('#textarea2, #buttonTwo').css("display","block");
+    $('#des-two').css("display", "none");
+    $('#textarea2, #buttonTwo').css("display", "block");
   });
-    $('#buttonTwo').on('click', function() {
-      $('#textarea2, #buttonTwo').css("display","none");
-      $('#des-two').html($('#textarea2').val());
-      $('#des-two').css("display","block");
+  $('#buttonTwo').on('click', function() {
+    $('#textarea2, #buttonTwo').css("display", "none");
+    $('#des-two').html($('#textarea2').val());
+    $('#des-two').css("display", "block");
   });
-//code for anything related to tiles
+  
+  //code for anything related to tiles
   $('#resetAll').on('click', function() {
-    $(".tiles").css('display','block');
+    $(".tiles").css('display', 'block');
   });
   $('#clearAll').on('click', function() {
-    $(".tiles").css('display','none');
+    $(".tiles").css('display', 'none');
   });
 
   $('.tiles').on('click', function(tile) {
-		$(this).fadeOut('1000', function(tile) {
-		});
-	});
-});//end of ready function
+    $(this).fadeOut('1000', function(tile) {});
+  });
+}); //end of ready function
